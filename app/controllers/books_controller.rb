6b6c2ddbec099baa6b316ab1787827fdcd469fe1
Geoglyph000 <nasca19970911@gmail.com>
2021-07-21 07:57:08 +0000

    Enable user-images on index etc, Update varidation:set min/max length

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,7 +21,8 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find(params[:id])
+    @book = Book.new
+    @post_book = Book.find(params[:id])
     @user = current_user
   end
 
@@ -32,7 +33,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
       if @book.update(book_params)
-      redirect_to book_path(@book), notice: "The post has been updated successfully."
+        redirect_to book_path(@book), notice: "The post has been updated successfully."
       else
         render :edit
       end
