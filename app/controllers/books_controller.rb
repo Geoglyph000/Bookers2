f3e0253eb38367d98dd0dec88200a7b73aee750d
Geoglyph000 <nasca19970911@gmail.com>
2021-07-24 01:48:43 +0000

    Remove unnecessary action and view: books/new

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,10 +5,6 @@ class BooksController < ApplicationController
     @user = current_user
   end
 
-  def new
-    @book = Book.new
-  end
-
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
