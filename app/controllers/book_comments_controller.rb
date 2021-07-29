007b524ece6d81563047d2a3460f9fc87f057f98
Geoglyph000 <nasca19970911@gmail.com>
2021-07-29 09:18:07 +0000

    [Finish]complete comment function

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,14 +1,23 @@
 class BookCommentsController < ApplicationController
+  before_action :authenticate_user!
 
   def create
     book = Book.find(params[:book_id])
-    comment = current.user.book_comments.new(book_comment_params)
-    comment.book_id = book_id
-    comment.save
-    redirect_to book_path(book)
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = book.id
+    if @book_comment.save
+      redirect_to book_path(book)
+    else
+      @book = Book.new
+      @post_book = Book.find(params[:book_id])
+      @user = @post_book.user
+      render "books/show"
+    end
   end
 
   def destroy
+    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    redirect_to book_path(params[:book_id])
   end
 
   private
