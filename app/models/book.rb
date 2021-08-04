4dcb243b28882c90a3b42016bb276b6c7843ad63
Geoglyph000 <nasca19970911@gmail.com>
2021-08-04 07:54:45 +0000

    update book search: enable to search with bodies as well

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,7 +10,7 @@ class Book < ApplicationRecord
 
   def self.search(search)
     return Book.all unless search
-    Book.where(['title LIKE ?',"%#{search}%"])
+    Book.where(['title LIKE(?) OR body LIKE(?)',"%#{search}%", "%#{search}%"])
   end
 
   validates :title, presence: true
