8311df8083c9c7c0172839f8019a802fd953146b
Geoglyph000 <nasca19970911@gmail.com>
2021-08-07 15:05:11 +0000

    Ajaxitize comment-posting

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,22 +2,22 @@ class BookCommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     @book_comment = current_user.book_comments.new(book_comment_params)
-    @book_comment.book_id = book.id
-    if @book_comment.save
-      redirect_to book_path(book)
+    @book_comment.book_id = @book.id
+    if @book_comment.save!
+      # redirect_to book_path(@book)
     else
       @book = Book.new
       @post_book = Book.find(params[:book_id])
       @user = @post_book.user
-      render "books/show"
+      # render "books/show"
     end
   end
 
   def destroy
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    # redirect_to book_path(params[:book_id])
   end
 
   private
