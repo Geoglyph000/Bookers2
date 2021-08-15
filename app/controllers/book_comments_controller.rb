14e852bb158738d9e1e6d64a9820d3bc108b688a
Geoglyph000 <nasca19970911@gmail.com>
2021-08-15 07:22:54 +0000

    right-justify search box

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -22,7 +22,7 @@ class BookCommentsController < ApplicationController
   end
 
   private
-  def book_comment_params
+  def book_comment_params #ストロング
     params.require(:book_comment).permit(:comment)
   end
 
